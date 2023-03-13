5dd5d4819d38b744347e5560ac1ecbee01fa7a1f
KLNHOMEALONE <KLNHOMEALONE@gmail.com>
2023-03-14 00:56:28 +0300

    Adding identity dapper postgres library

---
+++ CarRegistrationAPI/Data/Database.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using Npgsql;
+using System.Data.SqlClient;
+
+namespace CarRegistrationAPI.Data
+{
+    public static class Database
+    {
+        public static void EnsureDatabase(string connectionString, string name)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("name", name);
+            using var connection = new NpgsqlConnection(connectionString);
+            var records = connection.Query("SELECT 1 FROM pg_database WHERE datname= @name",
+                 parameters);
+            if (!records.Any())
+            {
+                connection.Execute($"CREATE DATABASE {name}");
+            }
+        }
+    }
+}
